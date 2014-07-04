ecab0f1a6f242e308a7fadff865aa3c5ec16893e
nbstr <nab.kml@gmail.com>
2014-07-04 14:36:35 +0200

    update

--- public/scripts/controller.js
+++ public/scripts/controller.js
@@ -12,18 +12,18 @@ function TerraceCtrl($scope, $http, $geo){
             .then(function(response){
                 $scope.DATA = response.data;
                 console.log($scope.DATA);
-                
-                return $scope.DATA;
-            });
 
-            $http.get(u('api/forecast?lat=' + position.coords.latitude + '&lng=' + position.coords.longitude))
-            // $http.get('data/forecast')
-            .error(function(response){
-                console.error(response);
-            })
-            .then(function(response){
-                $scope.FORECAST = response.data.data;
-                console.log($scope.FORECAST);
+                $http.get(u('api/forecast?lat=' + position.coords.latitude + '&lng=' + position.coords.longitude))
+                // $http.get('data/forecast')
+                .error(function(response){
+                    console.error(response);
+                })
+                .then(function(response){
+                    $scope.FORECAST = response.data.data;
+                    console.log($scope.FORECAST);
+                });
+                
+                //return $scope.DATA;
             });
 
         });
