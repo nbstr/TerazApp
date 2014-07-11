278464d624da0f6b114d86449a848e2ab3aa2e6f
nbstr <nab.kml@gmail.com>
2014-07-11 11:39:03 +0200

    Revert "jquery mobile"
    This reverts commit df26df8fde2f4de96d1732a898a4cc7354405791.

--- public/scripts/controller.js
+++ public/scripts/controller.js
@@ -1,6 +1,6 @@
 function TerraceCtrl($scope, $http, $geo){
 
-    $scope.get_data = function(radius, callback){
+    $scope.get_data = function(radius){
 
         $geo.position(function(position){
 
@@ -15,7 +15,6 @@ function TerraceCtrl($scope, $http, $geo){
                 if($scope.DATA.length > 0){
                     $scope.set_terrace($scope.DATA[0]);
                 }
-                // $(".list").iscrollview("refresh");
 
                 /*
                 $http.get(u('api/forecast?lat=' + position.coords.latitude + '&lng=' + position.coords.longitude))
@@ -28,37 +27,16 @@ function TerraceCtrl($scope, $http, $geo){
                 });
                 */
 
-                if(typeof(callback) === 'function'){
-                    callback();
-                }
-
             });
 
         });
 
     };
 
-    $scope.reload_data = function(callback){
+    $scope.reload_data = function(){
         var radius = document.getElementById('distance').value;
-        $scope.get_data(radius, callback);
+        $scope.get_data(radius);
     };
-
-    $(document).delegate("#list", "pageinit", function(event) {
-        $(".iscroll-wrapper", this).bind( {
-            iscroll_onpulldown : function(event, data){
-                alert('coucou');
-                $scope.reload_data(function(){
-                    data.iscrollview.refresh();
-                });
-            },
-            iscroll_onpullup   : function(event, data){
-                alert('coucou2');
-                $scope.reload_data(function(){
-                    data.iscrollview.refresh();
-                });
-            }
-        });
-    });
 
     $scope.slide_get = function(){
 
@@ -132,7 +110,6 @@ function TerraceCtrl($scope, $http, $geo){
 
         $scope.get_data(100000);
         $scope.initialize();
-
     };
     $scope.init();
 }
